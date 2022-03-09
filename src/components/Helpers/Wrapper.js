8acc479ef73e8beb7cf602e23ea888b0db92ea28
ali.abukahil <ali.abukahil@gmail.com>
2022-03-09 18:56:01 +0100

    React Fragments

--- src/components/Helpers/Wrapper.js
+++ src/components/Helpers/Wrapper.js
@@ -1,5 +1,5 @@
 // This basically an empty component
-// This dirty trick is done to avoid wrapping every component with a <div></div> HTML
+// This dirty trick is done to avoid wrapping every component with a <div></div> (so called (div soup)) HTML
 // Element/Tag or any other HTML Element/Tag because with the return you can only return one
 // Element not more than that, again The requirement just is, that there must be one root element
 
